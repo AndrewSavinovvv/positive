9ae99fd8c4f64f4b1a13349f03f70be90bcc5038
my eternal hunger <myeternalhunger@gmail.com>
2024-11-13 21:02:07 +0200

    Описание ваших изменений

---
+++ src/view/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainComponent from "@/components/MainComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import DialogComponent from "@/components/DialogComponent.vue";
+</script>
+
+<template>
+  <MainComponent></MainComponent>
+  <FooterComponent></FooterComponent>
+  <DialogComponent></DialogComponent>
+</template>
+
+<style scoped>
+
+</style>
